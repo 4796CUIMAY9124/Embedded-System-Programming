879c59ef25a7a153e71ffa02c5cdc8f216977a8d
May <73682011+4796CUIMAY9124@users.noreply.github.com>
2022-10-26 21:51:40 +0800

    Update IIC_plus.c

--- IIC/IIC_plus.c
+++ IIC/IIC_plus.c
@@ -1,6 +1,6 @@
-
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
 #include "driverlib/gpio.h"
@@ -14,7 +14,6 @@
 #include "utils/uartstdio.h"
 #include "driverlib/crc.h"
 #include "driverlib/i2c.h"
-
 
 
 //*****************************************************************************
@@ -31,10 +30,6 @@ float gy=0;
 float gz=0;
 float g=0;
 
-float zx=0;
-float zy=0;
-float zz=0;
-
 int ggx[3];
 int ggy[3];
 int ggz[3];
@@ -43,11 +38,9 @@ char sx[5];
 char sy[5];
 char sz[5];
 
-#define slaveAddress 0x53    //0x53好像是另一个可用从机地址
+#define slaveAddress 0x53      //另一个可用从机地址
 #define firstRegAddress 0x32   //DATAX0地址
-#define devid 0x00               //
-
-
+#define devid 0x00             //
 //*****************************************************************************
 //
 // The error routine that is called if the driver library encounters an error.
@@ -59,27 +52,11 @@ __error__(char *pcFilename, uint32_t ui32Line)
 {
 }
 #endif
-
 //*****************************************************************************
-//
-// Configure the UART and its pins.  This must be called before UARTprintf().
-//
-//*****************************************************************************
-
-
-
-//*****************************************************************************
-//
-// Print "Hello World!" to the UART on the Intelligent UART Module.
-//
-//*****************************************************************************
-
-
 uint16_t  SLAVE_rx_data;
 uint16_t  MASTER_rx_data[2];
-int
-main(void)
-                                      {
+int main(void)
+{
     //
     // Run from the PLL at 120 MHz.
     //
@@ -89,8 +66,7 @@ main(void)
 
     Lcd_init_basic();
 
-
-///////////////使能
+///////////////使能//////////////////
     SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 
@@ -106,84 +82,60 @@ main(void)
     I2CMasterInitExpClk(I2C0_BASE, g_ui32SysClock, false);//时钟使能
     I2CMasterIntClear(I2C0_BASE);//
 
-//从机打开&设置地址
-
-
-
-
     while(1)
     {
-        //读出ADXL345模块的内部寄存器0x00(DEVID)的值，用示波器记录SDA和
-        //SCL线上的波形，指出SDA上传输的数据的什么，哪部分是主机发送的，哪部分是从机发送的
-
-        //设置DATA_FORMAT寄存器和POWER_CTL寄存器寄存器，开始加速度的测量。读出DATAX0、DATAX1、DATAY0、DATAY1
-        //DATAZ0和DATAZ1寄存器的值，并转换位XYZ轴的加速度。
-
-        //把XYZ三个方向的重力加速度，显示在LCD显示屏上
-/*
         while(I2CMasterBusBusy(I2C0_BASE)){SysCtlDelay(400);};
-         while(I2CMasterBusy(I2C0_BASE)){SysCtlDelay(400);};
-         I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 0);
-         I2CMasterDataPut(I2C0_BASE,devid);//11100101
-         I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);//0x03
-         do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
-         I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 1);
-         I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);//0x07
-         do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
-         while(I2CMasterBusBusy(I2C0_BASE)){SysCtlDelay(400);};
-         de=I2CMasterDataGet(I2C0_BASE);
-         i=7;
-         I2CState=I2CMasterErr(I2C0_BASE);
-
-
-*/
-        //fahui
+        while(I2CMasterBusy(I2C0_BASE)){SysCtlDelay(400);};
+        I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 0);
+        I2CMasterDataPut(I2C0_BASE, 0x31);
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);//0x03
+        do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
+        I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 0);
+        I2CMasterDataPut(I2C0_BASE, 0x08);
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);//0x05
+        do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
         while(I2CMasterBusBusy(I2C0_BASE)){SysCtlDelay(400);};
-         while(I2CMasterBusy(I2C0_BASE)){SysCtlDelay(400);};
-         I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 0);
-         I2CMasterDataPut(I2C0_BASE, 0x31);
-         I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);//0x03
-         do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
-         I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 0);
-         I2CMasterDataPut(I2C0_BASE, 0x08);
-         I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);//0x05
-         do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
-         while(I2CMasterBusBusy(I2C0_BASE)){SysCtlDelay(400);};
         I2CState=I2CMasterErr(I2C0_BASE);
 
         while(I2CMasterBusBusy(I2C0_BASE)){SysCtlDelay(400);};
-                 while(I2CMasterBusy(I2C0_BASE)){SysCtlDelay(400);};
-                 I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 0);
-                 I2CMasterDataPut(I2C0_BASE, 0x2D);
-                 I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);//0x03
-                 do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
-                 I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 0);
-                 I2CMasterDataPut(I2C0_BASE, 0x08);
-                 I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);//0x05
-                 do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
-                 while(I2CMasterBusBusy(I2C0_BASE)){SysCtlDelay(400);};
-                I2CState=I2CMasterErr(I2C0_BASE);
+        while(I2CMasterBusy(I2C0_BASE)){SysCtlDelay(400);};
+        I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 0);
+        I2CMasterDataPut(I2C0_BASE, 0x2D);
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);//0x03
+        do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
+        I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 0);
+        I2CMasterDataPut(I2C0_BASE, 0x08);
+        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_FINISH);//0x05
+        do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
+        while(I2CMasterBusBusy(I2C0_BASE)){SysCtlDelay(400);};
+        I2CState=I2CMasterErr(I2C0_BASE);
 
         while(I2CMasterBusBusy(I2C0_BASE)){SysCtlDelay(400);};
-                 while(I2CMasterBusy(I2C0_BASE)){SysCtlDelay(400);};
+        while(I2CMasterBusy(I2C0_BASE)){SysCtlDelay(400);};
         //devid = HWREG(I2C0_BASE + 0x00) ;    //0x00偏移地址   读取DEVID寄存器的值
         //PB2 PB3 SDA SCL
         I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 0);
         I2CMasterDataPut(I2C0_BASE, firstRegAddress);
         I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START);
         do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
-        for(i=0;i<6;i++){
-        if(i==0){
-        I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 1);
-        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START);//0x0b
-        }else if(i==5){
-        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH);//0x05
-        }else{
-        I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_CONT);//0x09
-        }
-        do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
-        dataBuffer[i]=I2CMasterDataGet(I2C0_BASE);
-        I2CState=I2CMasterErr(I2C0_BASE);
+        for(i=0;i<6;i++)
+        {
+            if(i==0)
+            {
+                I2CMasterSlaveAddrSet(I2C0_BASE, slaveAddress, 1);
+                I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START);//0x0b
+            }
+            else if(i==5)
+            {
+                I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH);//0x05
+            }
+            else
+            {
+                I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_CONT);//0x09
+            }
+            do{SysCtlDelay(400);}while(I2CMasterBusy(I2C0_BASE));
+            dataBuffer[i]=I2CMasterDataGet(I2C0_BASE);
+            I2CState=I2CMasterErr(I2C0_BASE);
         }
 
         x = ((short)dataBuffer[1] << 8) + dataBuffer[0];
@@ -193,26 +145,17 @@ main(void)
         gy=y*0.004*9.8;
         gz=z*0.004*9.8;
 
-        if(gx<0) zx=-gx;
-        else zx=gx;
+        ggx[0]=(int)fabs(gx)/10;
+        ggy[0]=(int)fabs(gy)/10;
+        ggz[0]=(int)fabs(gz)/10;
 
-        if(gy<0) zy=-gy;
-                else zy=gy;
+        ggx[1]=(int)fabs(gx)-ggx[0]*10;
+        ggy[1]=(int)fabs(gy)-ggy[0]*10;
+        ggz[1]=(int)fabs(gz)-ggz[0]*10;
 
-        if(gz<0) zz=-gz;
-                else zz=gz;
-
-        ggx[0]=(int)zx/10;
-        ggy[0]=(int)zy/10;
-        ggz[0]=(int)zz/10;
-
-        ggx[1]=(int)zx-ggx[0]*10;
-        ggy[1]=(int)zy-ggy[0]*10;
-        ggz[1]=(int)zz-ggz[0]*10;
-
-        ggx[2]=(int)(zx*10)-ggx[0]*100-ggx[1]*10;
-        ggy[2]=(int)(zy*10)-ggy[0]*100-ggy[1]*10;
-        ggz[2]=(int)(zz*10)-ggz[0]*100-ggz[1]*10;
+        ggx[2]=(int)(fabs(gx)*10)-ggx[0]*100-ggx[1]*10;
+        ggy[2]=(int)(fabs(gy)*10)-ggy[0]*100-ggy[1]*10;
+        ggz[2]=(int)(fabs(gz)*10)-ggz[0]*100-ggz[1]*10;
 
         sx[0]=ggx[0]+48;
         sx[1]=ggx[1]+48;
@@ -235,13 +178,11 @@ main(void)
                 sz[4]='\0';
 
         LCD_gotoXY(1,0);
-       LCD_sendstr(sx);
-       LCD_gotoXY(2,0);
-      LCD_sendstr(sy);
+        LCD_sendstr(sx);
+        LCD_gotoXY(2,0);
+        LCD_sendstr(sy);
         LCD_gotoXY(3,0);
-      LCD_sendstr(sz);
-      delay(1*g_ui32SysClock);
-
+        LCD_sendstr(sz);
+        delay(1*g_ui32SysClock);
     }
-
 }
